349f227a1660e729f43cf5cf53cab6fcc3d81375
Nicolas Risso <105318465+NicolasRisso@users.noreply.github.com>
2026-04-02 14:27:19 -0300

    feat: initial project setup with raylib

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Arcade Survival - Get Started!");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Welcome to Arcade Survival!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
